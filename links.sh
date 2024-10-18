3881671aa13f09af2e4b1bc877ae684fa8efa2c4
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2024-10-18 17:42:43 -0300

    Copy my.cnf instead of linking
    Signed-off-by: mariasantosdev <mariaeduardasrodolfo2@gmail.com>

--- links.sh
+++ links.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ln -fns $PWD/zshrc $HOME/.zshrc
 ln -fns $PWD/alias $HOME/.alias
-ln -fns $PWD/my.cnf $HOME/.my.cnf
+sudo cp $PWD/my.cnf /etc/mysql/mysql.conf.d/custom.cnf && sudo chmod 644 /etc/mysql/mysql.conf.d/custom.cnf
